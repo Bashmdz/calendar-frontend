92b8f22909acbc89a1e7331e66a18e42c7207a71
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:47 +0500

    Update backend and frontend links in CONSTANT.jsx

--- src/CONSTANT.jsx
+++ src/CONSTANT.jsx
@@ -1,5 +1,5 @@
 export const CONSTANT = {
-  server: "http://127.0.0.1:8000/api/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
+  server: "http://127.0.0.1:8000/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
   admin: "http://127.0.0.1:8000/admin/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
   client: "http://localhost:5173/", // CHANGE WITH YOUR FRONTEND LINK (/ is MUST IN END)
 };
