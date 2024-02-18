a7288e919c69aef99a0c27fdc785b621461261e2
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:55 +0500

    Add session management using UserData context

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import UserData from "../contexts/UserData";
 export default function Layout(props) {
+  // ------------------
+  // SESSION - END
+  // ------------------
+  let __init_session = {
+    personal: {
+      id: "",
+      email: "",
+      name: "",
+    },
+    isLoggedIn: false,
+  };
+  const [session, setSession] = useState(__init_session);
+
+  useEffect(() => {
+    let sessionData = JSON.parse(sessionStorage.getItem("loggedin"));
+    if (sessionData) {
+      setSession({
+        ...__init_session,
+        personal: sessionData.data,
+        isLoggedIn: true,
+      });
+    }
+  }, []);
+
+  const value = { session, setSession };
+  // ------------------
+  // SESSION - END
+  // ------------------
   return (
     <>
-      <Header />
-      <div className="">{props.children}</div>
-      <Footer />
+      <UserData.Provider value={value}>
+        <Header />
+        <div className="">{props.children}</div>
+        <Footer />
+      </UserData.Provider>
     </>
   );
 }
