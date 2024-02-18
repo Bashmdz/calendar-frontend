d63bcecb9ff7677c72cd9eb2577294df632cc3a3
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:51 +0500

    Add header and footer components to Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 export default function Layout(props) {
   return (
     <>
+      <Header />
       <div className="">{props.children}</div>
+      <Footer />
     </>
   );
 }
