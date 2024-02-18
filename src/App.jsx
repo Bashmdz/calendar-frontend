f3a39de9c10ffa4ee9eab8ae459519968d006a17
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:59 +0500

    Update login path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             }
           />
           <Route
-            path="/login"
+            path="/signin"
             element={
               <Layout>
                 <Login />
