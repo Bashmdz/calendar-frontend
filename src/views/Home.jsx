f58853917c6b60c924f4b59b18cacc7c3cca8d3c
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:49 +0500

    Created Homepage view

---
+++ src/views/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>A Big Title</h1>
+      <p>
+        Welcome to Calendar App – your solution for managing tasks efficiently.
+        Keep track of your meetings, appointments, and to-do lists all in one
+        intuitive calendar interface. Stay organized, stay on top of your
+        schedule, and never miss an important date again!
+      </p>
+    </Container>
+  );
+};
+
+export default Home;
