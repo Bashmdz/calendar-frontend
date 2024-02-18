4179aca5d30779b75429b59e441ba20518653ed4
Rehan <148787477+Bashmdz@users.noreply.github.com>
2024-02-18 17:32:55 +0500

    Add login check functions to CONSTANT.jsx

--- src/CONSTANT.jsx
+++ src/CONSTANT.jsx
@@ -3,3 +3,17 @@ export const CONSTANT = {
   admin: "http://127.0.0.1:8000/admin/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
   client: "http://localhost:5173/", // CHANGE WITH YOUR FRONTEND LINK (/ is MUST IN END)
 };
+
+export const checkLoginFromLogin = () => {
+  return sessionStorage.getItem("loggedin") &&
+    JSON.parse(sessionStorage.getItem("loggedin")).data
+    ? true
+    : false;
+};
+
+export const checkLoginFromNonLogin = () => {
+  return sessionStorage.getItem("loggedin") &&
+    JSON.parse(sessionStorage.getItem("loggedin")).data
+    ? false
+    : true;
+};
