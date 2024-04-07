3c37288618a3dc767ad4560fb0f6233dcec88c8b
Bashmdz <bashar.zheiman@web.de>
2024-04-07 08:51:16 +0500

    Updated bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,41 +56,43 @@ const Header = (props) => {
           <span className="navbar-toggler-icon" />
         </button>
         {/* Navbar items */}
-        {isOpen && (
-          <div className="collapse navbar-collapse justify-content-end show">
-            <ul className="navbar-nav">
-              {props?.isLoggedIn ? (
-                <>
-                  {/* User info */}
-                  <li className="nav-item d-flex align-items-center justify-content-center me-2">
-                    <p className="text-muted m-0 p-0">
-                      Signed in as <b>{props?.personal?.name || "Anonymous"}</b>
-                    </p>
-                  </li>
-                  {/* Dashboard */}
-                  <ListItem label="Dashboard" to="/dashboard" />
-                  {/* Profile */}
-                  <ListItem label="Profile" to="/profile" />
-                  {/* Logout */}
-                  <ListItem
-                    label="Logout"
-                    onClick={() => {
-                      props?.logout();
-                      setIsOpen(false);
-                    }}
-                  />
-                </>
-              ) : (
-                <>
-                  {/* Register */}
-                  <ListItem label="Register" to="/register" />
-                  {/* Sign In */}
-                  <ListItem label="Sign In" to="/signin" />
-                </>
-              )}
-            </ul>
-          </div>
-        )}
+        <div
+          className={`collapse navbar-collapse justify-content-end md:show ${
+            isOpen ? "show" : ""
+          }`}
+        >
+          <ul className="navbar-nav">
+            {props?.isLoggedIn ? (
+              <>
+                {/* User info */}
+                <li className="nav-item d-flex align-items-center justify-content-center me-2">
+                  <p className="text-muted m-0 p-0">
+                    Signed in as <b>{props?.personal?.name || "Anonymous"}</b>
+                  </p>
+                </li>
+                {/* Dashboard */}
+                <ListItem label="Dashboard" to="/dashboard" />
+                {/* Profile */}
+                <ListItem label="Profile" to="/profile" />
+                {/* Logout */}
+                <ListItem
+                  label="Logout"
+                  onClick={() => {
+                    props?.logout();
+                    setIsOpen(false);
+                  }}
+                />
+              </>
+            ) : (
+              <>
+                {/* Register */}
+                <ListItem label="Register" to="/register" />
+                {/* Sign In */}
+                <ListItem label="Sign In" to="/signin" />
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
